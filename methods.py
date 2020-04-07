a4fba429688d321e292f80a593eeeab90ab200c1
Chris0084 <62215759+Chris0084@users.noreply.github.com>
2020-04-07 20:33:02 +0100

    count_cats method
    Added the count cats method that that returns the number of each unique catogory when a dataframe column is passed in

--- methods.py
+++ methods.py
@@ -1,2 +1,9 @@
 def test(x = "The methods.py module has loaded successfully"):
     print(x)
+    
+
+#Method that returns the number of each unique catogory when a dataframe column is passed in    
+def count_cats(dataframe_column):
+    counts = dataframe_column.value_counts()
+    return counts
+    
