474ac63843b397567201f0e2251e91c5bd8ffd03
Matthew Woolley <matthewwoolley31@gmail.com>
2020-04-09 03:30:21 +0100

    Added file detection and user-assignable CSV elements
    mt.load(True) tells the code it is in debug mode and automatically assigns values, however, mt.load() or mt.load(False) will allow the importing of different CSV files. The user will also be asked what the label title is as well as the descriptor data so they can have differently named CSV files. I also moved stripping to a seperate file to do something similar, however, it also allows uses the given titles from the intial setup to strip the data (minus the genre bit, that requires a discussion with Chris to decide on when this value is given)

--- methods.py
+++ methods.py
@@ -1,3 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import GridSearchCV
+import os.path
+
+location = ""
+label = ""
+descriptor = ""
+
 def test(x = "The methods.py module has loaded successfully"):
     print(x)
     
@@ -6,4 +20,35 @@ def test(x = "The methods.py module has loaded successfully"):
 def count_cats(dataframe_column):
     counts = dataframe_column.value_counts()
     return counts
+
+# Prompts a user for the file location (if debug = false) and then loads that file (if it exists)
+def load(debug = False):
+    global location
+    global label
+    global descriptor
+    # If this isn't in debug mode
+    if debug == False:
+        
+        while True:
+            location = input("What is the location of the csv file you wish to import?")
+            if os.path.exists(location):
+                break
+            else:
+                print("That file doesn't exist, please try again")
+                
+        label = input("What is the title of the column containing label data?")
+        descriptor = input("What is the title of the column containing the test data?")
     
+    else:
+        location = "Data/baselineMovies.csv"
+        label = "name"
+        descriptor = "overview"
+        
+    return pd.read_csv(location)
+
+def strip(data, evalColumn):
+    data_clean = data[pd.notnull(data[descriptor])]
+    templist = []
+    loopvariable = data_clean.genres
+
+    return [eval(y)[0][label]for y in data_clean[evalColumn]]
