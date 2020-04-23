bca8910ca14c1b62dc031e8e7df6f39b6ea84249
Matthew Woolley <matthewwoolley31@gmail.com>
2020-04-23 12:34:34 +0100

    Made the loading function easier to test

--- methods.py
+++ methods.py
@@ -14,44 +14,42 @@ descriptor = ""
 
 def test(x = "The methods.py module has loaded successfully"):
     print(x)
-    
 
-#Method that returns the number of each unique catogory when a dataframe column is passed in    
+
+#Method that returns the number of each unique catogory when a dataframe column is passed in
 def count_cats(dataframe_column):
     counts = dataframe_column.value_counts()
     return counts
 
 # Prompts a user for the file location (if debug = false) and then loads that file (if it exists)
-def load(debug = False):
+def load(location = ""):
     global location
     global label
     global descriptor
     # If this isn't in debug mode
-    if debug == False:
-        
+    if debug == "":
         while True:
-            location = input("What is the location of the csv file you wish to import?")
             if os.path.exists(location):
                 break
             else:
                 print("That file doesn't exist, please try again")
-                
+
         label = input("What is the title (or index) of the column containing label data?")
         descriptor = input("What is the title (or index) of the column containing the test data?")
-    
+
     else:
         location = "Data/baselineMovies.csv"
         label = "name"
         descriptor = "overview"
-     
+
     data = pd.read_csv(location)
-    
+
     if isinstance(label, int):
         label = data.columns[int(label)]
-        
+
     if isinstance(descriptor, int):
         descriptor = data.columns[int(descriptor)]
-        
+
     return data
 
 def strip(data, evalColumn):
